b2f9cd366d706a2beca92a0ff68d72291360d0db
nivashini <nivashinisambath@gmail.com>
2026-04-04 14:07:21 +0530

    Add files via upload

--- expense.js
+++ expense.js
@@ -6,23 +6,34 @@ let name = document.getElementById("name").value;
 let amount = document.getElementById("amount").value;
 let date = document.getElementById("date").value;
 
-if(name === "" || amount === "" || date === "") return;
+if(name === "" || amount === "" || date === ""){
+alert("Please fill all fields");
+return;
+}
 
 expenses.push({
-name:name,
-amount:amount,
-date:date
+name: name,
+amount: amount,
+date: date
 });
 
 localStorage.setItem("expenses", JSON.stringify(expenses));
 
 display();
 
+/* clear the input boxes after saving */
+
+document.getElementById("name").value = "";
+document.getElementById("amount").value = "";
+document.getElementById("date").value = "";
+
+document.getElementById("name").focus();
+
 }
 
-function deleteExpense(i){
+function deleteExpense(index){
 
-expenses.splice(i,1);
+expenses.splice(index,1);
 
 localStorage.setItem("expenses", JSON.stringify(expenses));
 
@@ -51,10 +62,15 @@ table.innerHTML += `
 
 }
 
+document.getElementById("name").addEventListener("input", function(){
+
+this.value = this.value.replace(/\b\w/g, function(char){
+return char.toUpperCase();
+});
+
+});
+
 function back(){
-
-window.location.href="index.html";
-
+window.location.href = "index.html";
 }
-
 display();
